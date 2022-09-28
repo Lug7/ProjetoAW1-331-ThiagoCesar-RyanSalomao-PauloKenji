cfedfcb6be48032abddc07de0edc0ad1fa98a131
Paulo Kenji <paulomuneischi@gmail.com>
2022-09-27 22:48:26 -0300

    removido arquivos temporarios

---
+++ scripts/Feedback.js
@@ -0,0 +1,23 @@
+var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var texto = document.getElementById("texto");
+
+var submit = document.getElementById("submit")
+
+submit.addEventListener("click", validação);
+
+function validação(){
+    if(nome.value == ""){
+        alert("O campo nome não pode estar vazio");
+        nome.focus();
+    }else
+    if(email.value == ""){
+        alert("O campo de email não pode estar vazio");
+        email.focus();
+    }
+    if(texto.value == ""){
+        alert("O campo do feedback não pode estar vazio");
+        texto.focus();
+    }else
+        alert("Feedback enviado com sucesso!!!");
+}
