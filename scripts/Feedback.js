629c4956bdfe60e632244fee7951b7ab7422d9b5
Ryan Salomão <ryansalomao@gmail.com>
2022-11-29 11:01:26 -0300

    add implementacao AJAX no feedback do usuário

--- scripts/Feedback.js
+++ scripts/Feedback.js
@@ -6,19 +6,25 @@ var texto = $("#texto")
 // submit.addEventListener("click", validação);
 $("#submit").click(validação);
 
-function validação(){
-    if(nome.val() == ""){
-        alert("O campo nome não pode estar vazio");
-        nome.focus();
-    }else
-    if(email.val() == ""){
-        alert("O campo de email não pode estar vazio");
-        email.focus();
+var xhr = new XMLHttpRequest(); 
+xhr.open('GET', "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"); 
 
-    }else
-    if(texto.val() == ""){
-        alert("O campo do feedback não pode estar vazio");
-        texto.focus();
-    }else
-        alert("Feedback enviado com sucesso!!!");
+xhr.onreadystatechange = function() { 
+    function validação(){
+        if(nome.val() == ""){
+            alert("O campo nome não pode estar vazio");
+            nome.focus();
+        }else
+        if(email.val() == ""){
+            alert("O campo de email não pode estar vazio");
+            email.focus();
+
+        }else
+        if(texto.val() == ""){
+            alert("O campo do feedback não pode estar vazio");
+            texto.focus();
+        }else
+            alert("Feedback enviado com sucesso!!!");
+        xhr.send();
+    }
 }
